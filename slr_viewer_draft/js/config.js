99f3391875b4603932f009b2aa067dff4a4f38d6
Kyrstin Fornace <klrfornace@gmail.com>
2023-01-09 15:31:50 -1000

    Clean up API key

--- slr_viewer_draft/js/config.js
+++ slr_viewer_draft/js/config.js
@@ -1,1 +1,1 @@
-const ak = 'pk.eyJ1Ijoia2Zvcm5hY2V1aCIsImEiOiJjbDh4aGo5ZTIwMGkyM3VxZ3p0YmJwaXFmIn0.ChPidstK8zfPVfSMEFH_BA';
+const ak = 'pk.eyJ1Ijoia2Zvcm5hY2V1aCIsImEiOiJjbGNqY2E5NzcwOWRxM3lwYzVyZXgxdTNtIn0.vUUr6kHAD3sLYujvNYCing';
